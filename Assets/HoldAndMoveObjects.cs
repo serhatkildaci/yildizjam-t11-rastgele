89b9851d0f1acc5df59bcb3ebfbd9ffb0853c86f
Serhat Kıldacı <kildaciserhat0@gmail.com>
2023-05-07 00:38:15 +0300

    flooryaptm

--- Assets/HoldAndMoveObjects.cs
+++ Assets/HoldAndMoveObjects.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class HoldAndMoveObjects : MonoBehaviour
 {
     public float objectDistance = 5f; // distance at which objects will be placed in front of camera
+    public float maxReachDistance = 10f; // maximum distance the player can hold and move objects
     public LayerMask objectLayer; // layer(s) on which objects can be selected
     public float minY = -10f; // minimum y value at which objects can be placed
     public float minObjectDistance = 1f; // Add a minimum distance variable
@@ -12,14 +13,11 @@ public class HoldAndMoveObjects : MonoBehaviour
 
     void Update()
     {
-
-
         if (Input.GetKeyDown(KeyCode.E))
         {
-
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, objectLayer))
+            if (Physics.Raycast(ray, out hit, maxReachDistance, objectLayer))
             {
                 AudioManager.instance.Play("Hold");
                 heldObject = hit.collider.gameObject;
@@ -32,13 +30,18 @@ public class HoldAndMoveObjects : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Vector3 newPosition = ray.origin + ray.direction * objectDistance + heldObjectOffset;
             newPosition.y = Mathf.Max(newPosition.y, minY); // prevent object from moving below minY
-            heldObject.transform.position = newPosition;
+            float distanceToPlayer = Vector3.Distance(transform.position, newPosition);
+            if (distanceToPlayer <= maxReachDistance)
+            {
+                heldObject.transform.position = newPosition;
+            }
         }
 
         if (Input.GetKeyUp(KeyCode.E))
         {
             heldObject = null;
         }
+
         float rotateSpeed = 5.0f; // adjust as needed
         float rotationAmount = Input.mouseScrollDelta.y * rotateSpeed;
 
@@ -47,6 +50,5 @@ public class HoldAndMoveObjects : MonoBehaviour
             Vector3 currentRotation = heldObject.transform.rotation.eulerAngles;
             heldObject.transform.rotation = Quaternion.Euler(0.0f, currentRotation.y + rotationAmount, 0.0f);
         }
-
     }
 }
