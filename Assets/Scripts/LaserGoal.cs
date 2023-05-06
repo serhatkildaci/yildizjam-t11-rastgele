c8587b4e8d59051e1ef6557ddc5d34cb327e98ed
Serhat Kıldacı <kildaciserhat0@gmail.com>
2023-05-07 01:08:38 +0300

    son hali map

--- Assets/Scripts/LaserGoal.cs
+++ Assets/Scripts/LaserGoal.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
 public class LaserGoal : MonoBehaviour
-{
+{   
+
+    public GameObject teleportobject;
     public GameObject doorToBeOpened;
     public GameObject player;  // reference to the player's game object
     public bool trigger = false;
@@ -16,7 +18,7 @@ public class LaserGoal : MonoBehaviour
     {
         if (trigger)
         {
-            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y-1f, player.transform.position.z); // teleport the player -10 units along the y-axis
+            player.transform.position = new Vector3(teleportobject.transform.position.x, teleportobject.transform.position.y, teleportobject.transform.position.z); // teleport the player -10 units along the y-axis
         }
     }
 
